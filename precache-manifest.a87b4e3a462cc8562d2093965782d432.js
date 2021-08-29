6e4de82cfa563641a94fcf8265614adcfd0f96cd
Andriypt82 <andriyivanyk82@gmail.com>
2021-08-29 23:17:05 +0100

    Updates

---
+++ precache-manifest.a87b4e3a462cc8562d2093965782d432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48d7aebf3131d0c501cdaaca991d1b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2627e81849d63b257a6c",
+    "url": "./static/css/main.48cfbad7.chunk.css"
+  },
+  {
+    "revision": "f0c4aff6c12a4f3db6f6",
+    "url": "./static/js/2.ca3efe42.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.ca3efe42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2627e81849d63b257a6c",
+    "url": "./static/js/main.2408301a.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
